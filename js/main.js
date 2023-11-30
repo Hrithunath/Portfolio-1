6305b5acf756f90def46d7dbaf780728ddb5e865
hrithunath <hrithunath777@gmail.com>
2023-11-30 22:37:56 +0530

    final

--- js/main.js
+++ js/main.js
@@ -308,3 +308,15 @@
 
 
 }());
+function validateForm(){
+	var name = document.getElementById('name').value;
+	var email = document.getElementById('email').value;
+	var subject = document.getElementById('subject').value;
+	var message = document.getElementById('message').value;
+	if (name.trim() === '' || email.trim() === '' || subject.trim() === ''
+	||message.trim() === ''){
+		aleret('Please fill out all fields.');
+		return false;
+	}
+	return true;
+}
